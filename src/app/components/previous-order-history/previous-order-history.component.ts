be78059d80097874c7eb20f704e52cecd6e77893
Muhammad Awais <muhammad.awais@royalcyber.com>
2024-09-17 14:07:57 +0500

    Previous Orders done

--- src/app/components/previous-order-history/previous-order-history.component.ts
+++ src/app/components/previous-order-history/previous-order-history.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { MockDataService } from '../../services/mock-data/mock-data.service';
+import { FormsModule, NgModel } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-previous-order-history',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './previous-order-history.component.html',
   styleUrl: './previous-order-history.component.scss',
 })
 export class PreviousOrderHistoryComponent implements OnInit {
   previousOrders: any[] = [];
+  filteredOrders: any[] = [];
+  searchTerm: string = '';
 
-  constructor(private mockDataService: MockDataService) {}
+  constructor(private mockDataService: MockDataService) { }
 
   ngOnInit(): void {
     this.previousOrders = this.mockDataService.getPreviousOrders();
+    this.filteredOrders = this.previousOrders;
+  }
+
+  filterOrders() {
+    if (!this.searchTerm) {
+      this.filteredOrders = this.previousOrders;
+    } else {
+      this.filteredOrders = this.previousOrders.filter(order =>
+        order.asset.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 }
