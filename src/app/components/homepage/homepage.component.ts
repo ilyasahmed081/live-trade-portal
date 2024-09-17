5208ceb6f873c72933c8a9f9ada14e33e145b13b
Muhammad Awais <muhammad.awais@royalcyber.com>
2024-09-17 15:55:14 +0500

    Withdrawal History chart on dashboard

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -12,11 +12,13 @@ import * as am4core from '@amcharts/amcharts4/core';
 })
 export class HomepageComponent implements OnInit, OnDestroy {
   private chart: am4charts.XYChart | undefined;
+  private withdrawalHistoryChart: am4charts.XYChart | undefined;
 
   constructor(private mockDataService: MockDataService) { }
 
   ngOnInit() {
     this.renderLiveTradesChart();
+    this.renderWithdrawalHistoryChart();
   }
 
   renderLiveTradesChart() {
@@ -57,6 +59,44 @@ export class HomepageComponent implements OnInit, OnDestroy {
     chart.legend = new am4charts.Legend();
 
     this.chart = chart;
+  }
+
+  renderWithdrawalHistoryChart() {
+    const withdrawalData = this.mockDataService.getWithdrawals();
+
+    let chart = am4core.create('withdrawalHistoryChartDiv', am4charts.XYChart);
+
+    // Transform the data for the chart
+    chart.data = withdrawalData.map((withdrawal) => ({
+      asset: withdrawal.asset,
+      date: withdrawal.date,
+      amount: withdrawal.amount,
+    }));
+
+    // Create date axis
+    let dateAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    dateAxis.dataFields.category = 'date';
+    dateAxis.title.text = 'Date';
+
+    // Create amount axis
+    let amountAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    amountAxis.title.text = 'Amount';
+
+    // Create series for each withdrawal
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = 'amount';
+    series.dataFields.categoryX = 'date';
+    series.name = 'Withdrawal Amount';
+    series.columns.template.tooltipText = '{name}: [bold]{valueY}[/]';
+    series.columns.template.fillOpacity = 0.8;
+
+    let columnTemplate = series.columns.template;
+    columnTemplate.strokeWidth = 2;
+    columnTemplate.strokeOpacity = 1;
+
+    // Add legend
+    chart.legend = new am4charts.Legend();
+    this.withdrawalHistoryChart = chart;
   }
 
   ngOnDestroy() {
